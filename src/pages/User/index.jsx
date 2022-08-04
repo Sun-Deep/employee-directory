57a62702a2c5a85e6319cdb2e5a862cc377d690d
Sun-Deep <sundeepp29@gmail.com>
2022-08-04 13:24:25 +0545

    add: fe search

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,29 +1,36 @@
-import { Button, Icon, VStack } from '@chakra-ui/react'
+import { Button, Flex, Icon, Input, VStack } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import CustomHeading from '../../components/CustomHeading'
 import CustomTable from '../../components/CustomTable'
+import { useToastError } from '../../components/Toast'
+
 import { getUsersList } from '../../services/users'
 
 const Users = () => {
   const [users, setUsers] = useState([])
+  const [finalUserList, setFinalUserList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [isRefresh, setIsRefresh] = useState(false)
 
   const navigate = useNavigate()
+  const toastError = useToastError()
 
   const getUsers = async () => {
     let _users = await getUsersList()
     setIsLoading(false)
     setUsers(_users?.data)
+    setFinalUserList(_users?.data)
   }
 
   useEffect(() => {
     setIsLoading(true)
     getUsers().catch((err) => {
       setIsLoading(false)
-      console.log(err)
+      toastError({
+        title: err,
+      })
     })
   }, [])
 
@@ -31,27 +38,64 @@ const Users = () => {
     if (isRefresh) {
       getUsers().catch((err) => {
         setIsLoading(false)
-        console.log(err)
+        toastError({
+          title: err,
+        })
       })
     }
   }, [isRefresh])
 
+  const handleSearch = (event) => {
+    const value = event.target.value
+    if (value !== '' && users.length > 0) {
+      let _u = users.filter((u) => {
+        if (
+          u.name.toLowerCase().includes(value.toLowerCase()) ||
+          u.job_title.toLowerCase().includes(value.toLowerCase()) ||
+          u.department.toLowerCase().includes(value.toLowerCase()) ||
+          u.location.toLowerCase().includes(value.toLowerCase())
+        ) {
+          return true
+        } else {
+          return false
+        }
+      })
+      setFinalUserList(_u)
+    } else if (value === '') {
+      setFinalUserList(users)
+    }
+  }
+
   return (
     <VStack spacing={5} w={'full'}>
       <CustomHeading title={'Employee List'} />
-      <Button
-        alignSelf={'start'}
-        leftIcon={<Icon as={FaPlus} />}
-        variant="primary"
-        size={'sm'}
-        onClick={() => navigate('/register')}
+      <Flex
+        justifyContent={'space-between'}
+        gridGap={5}
+        flexDir={['column', 'column', 'row']}
+        w={'full'}
       >
-        Add Emmployee
-      </Button>
+        <Button
+          alignSelf={'start'}
+          leftIcon={<Icon as={FaPlus} />}
+          variant="primary"
+          size={'sm'}
+          onClick={() => navigate('/register')}
+        >
+          Add Emmployee
+        </Button>
+        <Input
+          size={'sm'}
+          w={['full', 'full', '2xl', '3xl']}
+          placeholder="Search by name, title, department, location"
+          onChange={handleSearch}
+        />
+      </Flex>
+
       <CustomTable
         setIsRefresh={setIsRefresh}
         isLoading={isLoading}
-        users={users}
+        users={finalUserList}
       />
     </VStack>
   )
